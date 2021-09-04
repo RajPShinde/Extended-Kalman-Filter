08c34d16b1368f3031deb3694255a4ff4e09c328
RajPShinde <rajprakashshinde07@gmail.com>
2021-09-04 05:39:26 -0700

    Add subscribers for sensor data and method to store measurements

--- include/interface.hpp
+++ include/interface.hpp
@@ -2,7 +2,14 @@
 #define INCLUDE_INTERFACE_HPP_
 
 #include <extendedKalmanFilter.hpp>
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <visualization.hpp>
+#include <measurement.hpp>
+#include <utilities.hpp>
+#include <Eigen/Dense>
+#include <ros/ros.h>
 
 class Interface
 {
@@ -13,7 +20,7 @@ class Interface
         *   @param none
         *   @return none
         */
-        Interface();
+        Interface(ros::NodeHandle& nh, bool imu, bool odometry);
 
         /**
         *   @brief Destructor of Interface
@@ -24,18 +31,35 @@ class Interface
 
         void getMeasurement();
 
+        void storeMeasurement(Eigen::VectorXd &measurement,  Eigen::VectorXd &measurementCovariance, std::vector<int> data);
+
         void fuseData();
 
-        void removeIMUGravitationalAcceleration();
+        void publishOdometry(Eigen::VectorXd states);
 
-        void publishTransform();
+        void publishTransform(Eigen::VectorXd states);
+
+        void callbackIMU(const sensor_msgs::Imu &msg);
+        
+        // void callbackOdometry(nav_msgs::Odometry &msg);
 
     private:
 
-        ExtendedKalmanFilter ekf;
-        Visualization view;
+        ros::NodeHandle interface_;
+        ExtendedKalmanFilter ekf_;
+        Visualization view_;
+        Utilities data_;
         bool visualizeModel_, publishTransform_, useIMU_, useOdometry_ = true;
-        int totalSources_ = 2;
+        int queueIMU_, queueOdometry_ = 1;
+        ros::Subscriber odometrySub_;
+        ros::Subscriber imuSub_;
+        ros::Publisher odometryPub_;
+        int stateSize_ = 15;
+        std::vector<Measurement> sensorMeasurements_;
+        Measurement sensor_;
+        std::vector<int> dataIMU = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1};  // x y z r p y vx vy vz vr vp vy ax ay az
+        std::vector<int> dataOdometry = {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0}; // x y z r p y vx vy vz vr vp vy ax ay az
+        bool firstMeasurement = false;
 };
 
 #endif  //  INCLUDE_INTERFACE_HPP_
--- src/interface.cpp
+++ src/interface.cpp
@@ -1,27 +1,42 @@
 #include <interface.hpp>
 
 Interface::Interface(ros::NodeHandle& nh, bool imu, bool odometry): interface_(nh), useIMU_(imu), useOdometry_(odometry) {
-    totalSources_ = (useIMU_ && useOdometry_) ? 2 : 1;
+    ROS_WARN_STREAM("Interface");
+    imuSub_ = interface_.subscribe("/imu/data", 1, &Interface::callbackIMU, this);
+    // odomSub_ = interface_.subscribe("/odom", queueOdometry_, &Interface::callbackOdometry, this);
+    odometryPub_ = interface_.advertise<nav_msgs::Odometry>("/odometry/fused",10);
+    fuseData();
 }
 
 Interface::~Interface() {
 }
 
-void Interface::getMeasurement(){
+void Interface::callbackIMU(const sensor_msgs::Imu &msg){
+    ROS_WARN_STREAM("IMU Callback");
+    Eigen::VectorXd measurement(stateSize_);
+    Eigen::VectorXd measurementCovariance(stateSize_);
+    measurement.setZero();
+    measurementCovariance.setZero();
+    data_.getAccelerationMeasurement(msg, ekf_, measurement, measurementCovariance);
+    storeMeasurement(measurement, measurementCovariance, dataIMU);
+    firstMeasurement = true;
+}
+
+// void Interface::callbackOdometry(nav_msgs::Odometry &msg){
+//     Eigen::VectorXd measurement(stateSize_);
+//     Eigen::MatrixXd measurementCovariance(stateSize_, stateSize_);
+//     measurement.setZero();
+//     measurementCovariance.setZero();
+//     data_.getOdometryMeasurement(msg, ekf_, measurement, measurementCovariance);
+//     storeMeasurement(measurement, measurementCovariance, dataOdometry);
+// }
+
+void Interface::storeMeasurement(Eigen::VectorXd &measurements, Eigen::VectorXd &measurementCovariances, std::vector<int> data){
+    sensor_.measurements = measurements;
+    sensor_.measurementCovariances = measurementCovariances;
+    sensor_.dataToUse = data;
+    sensorMeasurements_.push_back(sensor_);
 }
 
 void Interface::fuseData(){
-    while(ros::ok()){
-        ekf.predict();
-        ekf.update();
-    }
-}
-
-void Interface::removeIMUGravitationalAcceleration(){
-}
-
-void Interface::publishTransform(){
-    if(visualizeModel_){
-        view.visualizeModel();
-    }
 }
